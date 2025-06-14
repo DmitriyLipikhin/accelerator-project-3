97e4b36dbf7f187ccb41b36ed9eb617e874f5cf3
Dmitriy <dimanich12512@gmail.com>
2025-06-14 20:51:38 +1000

    1

--- source/js/swiper-news.js
+++ source/js/swiper-news.js
@@ -4,13 +4,12 @@ import {Pagination, Mousewheel, Navigation, Scrollbar, Grid} from 'swiper/module
 function reorderSlidesForGrid(swiperContainer, rows = 2) {
   const wrapper = swiperContainer.querySelector('.swiper-wrapper');
   const slides = Array.from(wrapper.querySelectorAll('.swiper-slide'));
-  
-  if (slides.length <= rows) return; // Если слайдов меньше, чем строк, не меняем порядок
+
+  if (slides.length <= rows) return;
 
   const reorderedSlides = [];
   const slidesPerColumn = Math.ceil(slides.length / rows);
 
-  // Собираем новый порядок: [1, 3, 5..., 2, 4, 6...]
   for (let i = 0; i < slidesPerColumn; i++) {
     for (let row = 0; row < rows; row++) {
       const index = i * rows + row;
@@ -20,16 +19,65 @@ function reorderSlidesForGrid(swiperContainer, rows = 2) {
     }
   }
 
-  // Очищаем и вставляем слайды в новом порядке
   wrapper.innerHTML = '';
   reorderedSlides.forEach(slide => wrapper.appendChild(slide));
 }
 
-// Применяем перестановку перед инициализацией Swiper
 const swiperEl = document.querySelector('#swiper-news');
 if (swiperEl) {
-  reorderSlidesForGrid(swiperEl, 2); // 2 строки
+  reorderSlidesForGrid(swiperEl, 2);
 }
+
+window.addEventListener('DOMContentLoaded', function () {
+  const containersDescription = document.querySelectorAll('.news-card__text-subcontainer-description');
+  const containersTitle = document.querySelectorAll('.news-card__text-subcontainer-title');
+  const containersDate = document.querySelectorAll('.news-card__date-subcontainer');
+  const newsCard = document.querySelectorAll('.news-card');
+
+  containersDescription.forEach(function(container) {
+      let currentHeight = container.clientHeight || container.offsetHeight;
+
+      let newHeightValue = `${currentHeight - 24}px`;
+
+      container.style.setProperty('--nb-h', newHeightValue);
+
+      console.log(`Высота для элемента "${container.textContent}" стала: ${newHeightValue}`);
+  });
+
+  containersTitle.forEach(function(container) {
+    let currentHeight = container.clientHeight || container.offsetHeight;
+
+    let newHeightValue = `${currentHeight - 24}px`;
+
+    container.style.setProperty('--nb-h', newHeightValue);
+
+    console.log(`Высота для элемента "${container.textContent}" стала: ${newHeightValue}`);
+});
+
+containersDate.forEach(function(container) {
+  let currentHeight = container.clientHeight || container.offsetHeight;
+
+  let newHeightValue = `${currentHeight - 24}px`;
+
+  container.style.setProperty('--nb-h', newHeightValue);
+
+  console.log(`Высота для элемента "${container.textContent}" стала: ${newHeightValue}`);
+});
+
+newsCard.forEach(function(card) {
+  const textContainer = card.querySelector('.news-card__text-container');
+  const containerImage = card.querySelector('.news-card__image');
+
+  let currentHeight = textContainer.clientHeight || textContainer.offsetHeight;
+
+  let newHeightValue = `${currentHeight - 24}px`;
+
+  containerImage.style.setProperty('--nb-h', newHeightValue);
+
+  console.log(`Высота для элемента "${containerImage.textContent}" стала: ${newHeightValue}`);
+});
+
+});
 
   new Swiper ('#swiper-news', {
   modules: [Pagination, Mousewheel, Navigation, Scrollbar, Grid],
@@ -51,22 +99,26 @@ if (swiperEl) {
   },
 
   pagination: {
-    el: '.hero__slider-pagination',
+    el: '.news__pagination-wrapper',
     clickable: true,
     type: 'bullets',
+    bulletClass: 'news__pagination-button',
+    bulletActiveClass: 'news__pagination-button--active',
+    renderBullet: function (index, className) {
+      return `<button class="${className}">${index + 1}</button>`;
+    },
   },
 
   navigation: {
-    nextEl:'.programs__button-right',
-    prevEl:'.programs__button-left'
+    nextEl:'.news__pagination-button-arrow--right',
+    prevEl:'.news__pagination-button-arrow--left'
   },
 
    on: {
     init: function() {
-      const slides = this.slides; // все слайды
-      const half = Math.ceil(slides.length / 2); // округляем вверх (для 7 → 4)
-      
-      // Добавляем класс для второй половины
+      const slides = this.slides;
+      const half = Math.ceil(slides.length / 2);
+
       slides.forEach((slide, index) => {
         if (index >= half) {
           slide.classList.add('news-card--indent');
